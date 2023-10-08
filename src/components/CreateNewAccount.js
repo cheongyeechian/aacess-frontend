00eda9b5b9e38e5e9774529233827af06303165a
Benjamin <tankerkzheng1@gmail.com>
2023-10-08 14:26:51 +0800

    Update CreateNewAccount.js

--- src/components/CreateNewAccount.js
+++ src/components/CreateNewAccount.js
@@ -20,7 +20,37 @@ function CreateNewAccount({ setWallet, setSeedPhrase }) {
   }
   return (
     <>
-      <div className="content"></div>
+      <div className="content">
+        <div className="mnemonic">
+          <ExclamationCircleOutlined style={{ fontSize: "20px" }} />
+          <div>
+            Once you generate the seed phrase, save it securely in order to
+            recover your wallet in the future.
+          </div>
+        </div>
+        <Button
+          className="frontPageButton"
+          type="primary"
+          onClick={() => generateWallet()}
+        >
+          Generate Seed Phrase
+        </Button>
+        <Card className="seedPhraseContainer">
+          {newSeedPhrase && (
+            <pre style={{ whiteSpace: "pre-wrap" }}>{newSeedPhrase}</pre>
+          )}
+        </Card>
+        <Button
+          className="frontPageButton"
+          type="default"
+          onClick={() => setWalletAndMnemonic()}
+        >
+          Open Your New Wallet
+        </Button>
+        <p className="frontPageBottom" onClick={() => navigate("/")}>
+          Back Home
+        </p>
+      </div>
     </>
   );
 }
