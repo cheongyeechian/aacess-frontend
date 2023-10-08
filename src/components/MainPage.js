27e0d53762cefda623b62575c3bb2d13c7cf8d2a
Benjamin <tankerkzheng1@gmail.com>
2023-10-08 14:28:09 +0800

    Create MainPage.js

---
+++ src/components/MainPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+import mwallet from "../mwallet.png";
+import { Button } from "antd";
+import { useNavigate } from "react-router-dom";
+
+function MainPage() {}
+export default MainPage;
