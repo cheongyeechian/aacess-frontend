57b155fcbf414861a27cf03ab964627e90184587
Benjamin <tankerkzheng1@gmail.com>
2023-10-08 14:29:09 +0800

    Create LoginAccount.js

---
+++ src/components/LoginAccount.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { BulbOutlined } from "@ant-design/icons";
+import { Button, Input } from "antd";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { ethers } from "ethers";
+
+const { TextArea } = Input;
+
+function LoginAccount({ setWallet, setSeedPhrase }) {}
+export default LoginAccount;
