f200cd004b336d4d991deaef623974346f839b70
Benjamin <tankerkzheng1@gmail.com>
2023-10-08 14:30:13 +0800

    Create ViewWalletPage.js

---
+++ src/components/ViewWalletPage.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import {
+  Divider,
+  Tooltip,
+  List,
+  Avatar,
+  Spin,
+  Tabs,
+  Input,
+  Button,
+} from "antd";
+import { LogoutOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
+import logo from "../noImg.png";
+import axios from "axios";
+import { CHAINS_CONFIG } from "../chains";
+import { ethers } from "ethers";
+
+function ViewWalletPage() {}
+export default ViewWalletPage;
