d7101837e681817ca9cd22616a43a5d6994c7333
Cheong Yee Chian <cheongyeechian@gmail.com>
2023-10-11 13:23:00 +0800

    CorePage test

--- src/components/CorePage.js
+++ src/components/CorePage.js
@@ -1,1 +1,14 @@
 import React from "react";
+import { Button } from "antd";
+
+function CorePage(){
+    const navigate = useNavigate();
+    return(
+        <>
+        <div>
+            <h2>Hey There</h2>
+        </div>
+        </>
+    )
+}
+export default CorePage;
